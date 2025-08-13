d651316398b4e4d4130d6d0d5c76c54a110ace98
153server <139309287+153server@users.noreply.github.com>
2025-08-14 03:30:08 +0800

    Create DashboardLayout.d.ts

---
+++ dive-ops-supabase/components/DashboardLayout.d.ts
@@ -0,0 +1,6 @@
+// components/DashboardLayout.d.ts
+declare module '../components/DashboardLayout' {
+  import { ReactNode } from 'react';
+  declare const DashboardLayout: (props: { children: ReactNode }) => JSX.Element;
+  export default DashboardLayout;
+}
