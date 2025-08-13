5ab7cc3580aecd6709a9b560551fb94c200886d2
153server <139309287+153server@users.noreply.github.com>
2025-08-13 17:38:02 +0800

    Update next.config.js

--- dive-ops-supabase/next.config.js
+++ dive-ops-supabase/next.config.js
@@ -1,3 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = { reactStrictMode: true, images: { unoptimized: true } };
 module.exports = nextConfig;
+/pages/scan.tsx
+/components/Layout.tsx
+/scripts/seed.ts
